1ee65013d3f86e08389cceb97c03ffd6d3b9baac
Krishnanand A <krishnanand654@gmail.com>
2021-05-23 20:22:30 +0530

    Update Row.js

--- src/Row.js
+++ src/Row.js
@@ -21,8 +21,8 @@ function Row({ title, fetchUrl, isLargeRow }) {
     }, [fetchUrl]);
 
     const opts = {
-        height: '390',
-        width: '640',
+        height: '350px',
+        width: '100%',
         playerVars: {
           // https://developers.google.com/youtube/player_parameters
           autoplay: 1,
